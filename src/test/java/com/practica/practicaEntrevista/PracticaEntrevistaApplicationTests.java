0d5a66b2df55a0a66d930bc6fc0a2d46f1f9c2bf
deibydev <desierra@activos.com.co>
2023-03-27 17:31:36 -0500

    Proyecto Inicializado

---
+++ src/test/java/com/practica/practicaEntrevista/PracticaEntrevistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.practicaEntrevista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaEntrevistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
